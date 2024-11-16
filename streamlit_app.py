8d3ffea90cd99c52d7c18c64f1965ce66bbc8230
Isabelly Barbosa <110305808+Isabellycomex@users.noreply.github.com>
2024-11-16 16:03:43 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
-import plotly.express as px
+
 
 st.title("Análise de Discurso de ódio no Reddit através do ChatGpt")
 st.write("""
