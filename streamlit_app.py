ccbeb1934c27ecda8561389b8aa86f9a2b1156bf
Isabelly Barbosa <110305808+Isabellycomex@users.noreply.github.com>
2024-11-16 15:45:33 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,5 @@ import matplotlib.pyplot as plt
 import plotly.express as px
 
 st.title("Análise de Discurso de ódio no Reddit através do ChatGpt")
-st.write(
-    "O combate ao discurso de ódio é um compromisso coletivo que exige empatia, educação e a promoção de um diálogo respeitoso, pois é na palavra consciente que construímos pontes ao invés de muros."
-)
+
 
