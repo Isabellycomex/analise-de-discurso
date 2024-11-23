96fd57d8e3eb114b16248f9a60dcd0a1cd7311ef
Isabelly Barbosa <110305808+Isabellycomex@users.noreply.github.com>
2024-11-23 00:16:28 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -268,7 +268,8 @@ if "Palavras Mais Comuns em Discurso de Ódio" in visualizacoes:
             "suas", "me", "te", "nos", "vos", "depois", "antes", "até", "ainda", "hoje", "ontem", 
             "amanhã", "agora", "lá", "cá", "sim", "não", "pois", "porém", "como", "sobre", "entre", 
             "contra", "sem", "baixo", "apenas", "mesmo", "era", "só", "coisa", "ser", "pessoa", "pai", "cara", "tem", "bem",
-            "foi", "pessoas", "ser", "sou", "ano", "vc", "queria", "gente", "ao", "disse", "nunca", "sempre", "casa", "tempo"
+            "foi", "pessoas", "ser", "sou", "ano", "vc", "queria", "gente", "ao", "disse", "nunca", "sempre", "casa", "tempo",
+            "nem", "mim", "q", "que", "pq", "mãe"
         ])
 
         # Gerar nuvem de palavras
