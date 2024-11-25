acbb669971246958a04c706064802344a6bf2185
Isabelly Barbosa <110305808+Isabellycomex@users.noreply.github.com>
2024-11-24 23:39:47 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -276,32 +276,55 @@ if "Visualizações por Tipo de Discurso de Ódio" in visualizacoes:
         st.plotly_chart(fig_visualizacoes_tipo)
     else:
         st.write("Não há dados de discurso de ódio para exibir.")
+if "Palavras Mais Comuns em Discurso de Ódio" in visualizações:
+    # Filtrar os dados para considerar apenas discursos de ódio
+    data_odio = data_filtered[data_filtered["resultado_analise"] != "não é discurso de ódio"]
+    
+    # Verifique se há dados após o filtro
+    if not data_odio.empty:
+        from wordcloud import WordCloud
+        stop_words = set(STOPWORDS)
+        
+        stop_words.update([
+            "de", "como", "por", "mais", "quando", "se", "ele", "pra", "isso", "da", 
+            "para", "com", "que", "em", "é", "e", "o", "a", "os", "como", "um", "uma", 
+            "na", "no", "não", "mas", "ela", "eu", "você", "vocês", "nós", "eles", "elas", 
+            "meu", "minha", "meus", "minhas", "teu", "tua", "teus", "tuas", "dele", "dela", 
+            "deles", "delas", "esse", "essa", "esses", "essas", "este", "esta", "estes", 
+            "estas", "aquele", "aquela", "aqueles", "aquelas", "lhe", "lhes", "do", "dos", 
+            "das", "num", "numa", "neste", "nesta", "nisto", "naquele", "naquela", "nisso", 
+            "daquilo", "e", "ou", "onde", "porque", "porquê", "lá", "aqui", "ali", "assim", 
+            "tão", "já", "então", "também", "muito", "pouco", "sempre", "tudo", "nada", 
+            "cada", "todos", "todas", "algum", "alguma", "nenhum", "nenhuma", "outro", 
+            "outra", "outros", "outras", "seu", "sua", "seus", "suas", "me", "te", "nos", 
+            "vos", "depois", "antes", "até", "ainda", "hoje", "ontem", "amanhã", "agora", 
+            "lá", "cá", "sim", "não", "pois", "porém", "como", "sobre", "entre", "contra", 
+            "sem", "baixo", "apenas", "mesmo", "era", "só", "coisa", "ser", "pessoa", "pai", 
+            "cara", "tem", "bem", "foi", "pessoas", "ser", "sou", "ano", "vc", "queria", 
+            "gente", "ao", "disse", "nunca", "sempre", "casa", "tempo", "nem", "mim", "q", 
+            "que", "pq", "mãe", "mulher", "sala", "dia", "estava", "tenho", "vai", "começou", 
+            "fazer", "são", "amigo", "namorada", "anos", "ter", "enquanto", "homem", "aí", 
+            "tinha", "vida", "estou", "grupo", "coisas", "fui"
+        ])
+        
+        # Gerar nuvem de palavras
+        wordcloud = WordCloud(
+            background_color="black",
+            stopwords=stop_words,
+            colormap="coolwarm",
+            width=800,
+            height=400
+        ).generate(textos)
 
- if "Palavras Mais Comuns em Discurso de Ódio" in visualizações: 
- # Filtrar os dados para considerar apenas discursos de ódio data_odio = data_filtered[data_filtered["resultado_analise"] != " não é discurso de ódio"] 
-     # Verifique se há dados após o filtro if not data_odio.empty: from wordcloud import WordCloud, 
-     stop_words = set(STOPWORDS) stop_words.update([ "de", "como", "por", "mais ", "quando", "se", "ele", "pra", "isso", "da", 
-    "para", "com", "que", "em", "é", "e", "o", "a", "os", "como", "um", "uma", "na", "no", "não", "mas", "ela", "eu", "você", 
-    "vocês", "nós", "eles", "elas", "meu", "minha", "meus", "minhas", "teu", "tua", "teus", "tuas", "dele", "dela", 
-    "deles", "delas", "esse", "essa", "esses", "essas", "este", "esta", "estes", "estas", "aquele", "aquela", 
-    "aqueles", "aquelas", "lhe", "lhes", "do", "dos", "das", "num", "numa", "neste", "nesta", "nisto ", "naquele", "naquela", 
-    "nisso", "daquilo", "e", "ou", "onde", "porque", "porquê", "lá", "aqui", "ali", "assim", "tão", "já", "então", "também", 
-    "muito", "pouco", "sempre", "tudo", "nada", "cada", "todos", "todas", "algum", "alguma", "nenhum", "nenhuma", "outro", "outra", 
-    "outros", "outras", "seu", "sua", "seus", "suas", "me", "te", "nos", "vos", "depois", "antes", "até", "ainda", "hoje", "ontem", 
-    "amanhã", "agora", "lá", "cá", "sim", "não", "pois", "porém", "como", "sobre", "entre", "contra", "sem", "baixo", "apenas", "mesmo", 
-    "era", "só", "coisa ", "ser", "pessoa", "pai", "cara", "tem", "bem", "foi", "pessoas", "ser", "sou", "ano","vc", "queria", "gente",
-    "ao", "disse", "nunca", "sempre", "casa", "tempo", "nem", "mim", "q", "que ", "pq", "mãe", "mulher", "sala", "dia", "estava", "tenho", 
-    "vai", "começou", "fazer", "são", "amigo", "namorada", "anos", "ter", "enquanto", "homem", "aí", "tinha", "vida", "estou", "grupo", "coisas", "fui" ])
-     # Gerar nuvem de palavras wordcloud = WordCloud( 
-                            background_color="black", 
-                            stopwords=stop_words, 
-                            colormap="coolwarm", 
-                            width=800, height=400 ).generate(textos) # Exibir gráficos no Streamlit 
-    fig6, ax = plt.subplots(figsize=(10 , 5)) 
-    ax.imshow(wordcloud, interpolation="bilinear")
-    ax.axis("off") ax.set_title("Palavras Mais Comuns em Discurso de Ódio", fontsize=18, color="white") 
+        # Exibir gráficos no Streamlit
+        fig6, ax = plt.subplots(figsize=(10, 5))
+        ax.imshow(wordcloud, interpolation="bilinear")
+        ax.axis("off")
+        ax.set_title("Palavras Mais Comuns em Discurso de Ódio", fontsize=18, color="white")
 
-st.pyplot(fig6) else: st.write("Não há dados de discurso de ódio para gerar a nuvem de palavras.")
+        st.pyplot(fig6)
+    else:
+        st.write("Não há dados de discurso de ódio para gerar a nuvem de palavras.")
 
 
 
