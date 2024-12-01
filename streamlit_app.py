ab59fdf8828a9e229d6eb63614a5cc7a6b7eba94
Isabelly Barbosa <110305808+Isabellycomex@users.noreply.github.com>
2024-12-01 00:17:37 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -303,7 +303,7 @@ else:
     raise ValueError("A coluna 'hora_postagem' não foi encontrada.")
 
 # Filtrar os dados para discurso de ódio
-if "Frequência por Tipo de Discurso" in visualizacoes:
+if "Frequência por tipo de discurso" in visualizacoes:
     # Filtrar os dados com base no tipo de discurso
     odio_tempo = data_filtered[data_filtered["resultado_analise"].isin(["racismo", "homofobia", "sexismo", "xenofobia", "transfobia", "não é discurso de ódio"])]
     
