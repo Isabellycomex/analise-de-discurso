16385f849d59e636f37e88519820c76302dab916
Isabelly Barbosa <110305808+Isabellycomex@users.noreply.github.com>
2024-12-06 21:55:22 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -144,7 +144,7 @@ ITENS_POR_PAGINA = 10
 
 # Configuração para limitar a navegação
 PAGINA_MINIMA = 1
-PAGINA_MAXIMA = 31
+PAGINA_MAXIMA = 41
 
 # Verificar se o DataFrame filtrado não está vazio
 if not data_filtered.empty:
@@ -165,7 +165,7 @@ if not data_filtered.empty:
         - Use os botões **Próximo** e **Anterior** para navegar entre as páginas.
         - Role a tabela para **baixo** ou para os **lados** para ver mais detalhes das publicações.
         - Cada página exibe até **10 publicações**.
-        - Navegação limitada às páginas **1 a 31**.
+        - Navegação limitada às páginas **1 a 41**.
         - Clique no **campo** que deseja visualizar para verificar todos os dados do mesmo.
         """
     )
